85bb9e1afc428d83fafe4ff85ce5ee53ea46daf3
Auios <LoneAuios@gmail.com>
2015-11-14 17:09:59 -0500

    Initial

---
+++ DataGrapher.bat
@@ -0,0 +1,2 @@
+CD C:\program files\git
+git-cmd gource C:\users\%USERNAME%\documents\github\datagrapher -f -1920x1080 -c 1 --disable-auto-rotate --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 1 --seconds-per-day 1
