c24564fa9e8c9897340cd8293714bb4371e5661d
Auios <LoneAuios@gmail.com>
2016-10-12 02:34:35 -0400

    Test

--- FreeBasic_Stuff.bat
+++ FreeBasic_Stuff.bat
@@ -1,2 +1,2 @@
-CD C:\program files\git
+CD D:\Programs\git
 git-cmd gource C:\users\%USERNAME%\documents\github\FreeBasic_Stuff -f -1920x1080 -c 1 --disable-auto-rotate --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 1 --seconds-per-day 1
