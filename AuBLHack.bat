ac660988bd2e188b4198047648471d17c9b961a5
Auios <LoneAuios@gmail.com>
2016-12-10 16:51:06 -0500

    D

--- AuBLHack.bat
+++ AuBLHack.bat
@@ -1,2 +1,3 @@
-CD C:\program files\git
+D:
+CD D:\programs\git
 git-cmd gource C:\users\%USERNAME%\documents\github\AuBLHack -f -1920x1080 -c 1 --disable-auto-rotate --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 1 --seconds-per-day 1
