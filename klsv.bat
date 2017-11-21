a698024aa7d2cb363d086bb418e675afffd0fdb8
Auios <LoneAuios@gmail.com>
2017-11-21 06:31:29 -0500

    Keyloggers

---
+++ klsv.bat
@@ -0,0 +1,2 @@
+CD C:\users\%USERNAME%\documents\github\KeyLoggerNet_Server
+gource -f -1920x1080 -c 1 --disable-auto-rotate --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 1 --seconds-per-day 0.5
