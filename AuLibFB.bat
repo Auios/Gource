e938d647ff39dd88830574e60765d74a226a32bc
Auios <LoneAuios@gmail.com>
2019-04-15 16:17:10 -0400

    Update AuLibFB.bat

--- AuLibFB.bat
+++ AuLibFB.bat
@@ -1,2 +1,3 @@
 CD ..\AuLibFB
 gource -f -1920x1080 -c 1 --disable-auto-rotate --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 0.5 --seconds-per-day 0.5
+pause
