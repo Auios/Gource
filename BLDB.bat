a67957db09c62ad25bebd6fe6ef270ae0b3531af
Auios <LoneAuios@gmail.com>
2020-11-02 16:52:53 -0600

    Update BLDB.bat

--- BLDB.bat
+++ BLDB.bat
@@ -1,2 +1,2 @@
 CD ..\BLDB
-gource -f -1920x1080 -c 1 --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 1 --seconds-per-day 2.0
+gource -f -1920x1080 -c 1 --font-size 26 --hide mouse,progress --highlight-users --multi-sampling --auto-skip-seconds 0.5 --seconds-per-day 0.2
